e183475ad3e0ff0d6754e4227c1c5679d8ecda73
Hoyong Jung <hoyong.jung.dev@gmail.com>
2021-02-25 12:11:25 +0900

    add test case of test_find_clump

--- chapter1/test_chapter1.py
+++ chapter1/test_chapter1.py
@@ -8,6 +8,7 @@ class TestChapter1(unittest.TestCase):
 
     def test_find_clump(self):
         self.assertListEqual(find_clump('AAAACGTCGAAAAA', 2, 4, 2), ['AA'])
+        self.assertListEqual(sorted(find_clump('ACGTACGT', 1, 5, 2)), ['A', 'C', 'G', 'T'])
 
     def test_frequent_words(self):
         self.assertListEqual(frequent_words('ACAACTATGCATCACTATCGGGAACTATCCT', 5), ['ACTAT'])
